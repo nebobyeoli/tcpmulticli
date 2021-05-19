f2d3fa67299afacec3c2582416546ea3d65232da
nebobyeoli <nikkilover17@naver.com>
2021-05-19 11:23:29 +0900

    PROCESS: Handling multiple msg BETWEEN clients

--- mulcli.c
+++ mulcli.c
@@ -1,4 +1,5 @@
 // ECHO CLIENT
+// + MULTICAST RECEIVE
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,6 +7,8 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <sys/select.h>
+#include <sys/time.h>
 
 #define BUF_SIZE 1024
 
@@ -17,6 +20,21 @@ void error_handling(char *message)
 
 int main(int argc, char *argv[])
 {
+    // FD
+    int fd, state;
+    char buf[BUF_SIZE];
+
+    struct  timeval tr;  // timeval_read
+    struct  timeval ts;  // timeval_send
+    fd_set  readfds;
+
+    fd = fileno(stdin);
+
+
+    enum { SEND, RECEIVE } MODE;
+    MODE = SEND;
+    
+    
 	int sock;
 	char message[BUF_SIZE];
 	int str_len;
@@ -34,22 +52,136 @@ int main(int argc, char *argv[])
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
 	serv_addr.sin_port = htons(atoi(argv[2]));
+
+    tr.tv_sec = 0;
+    tr.tv_usec = 100000;  // 1000000 usec = 1 sec
+
+    // https://stackoverflow.com/a/2939145
+    setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tr, sizeof tr);
 	
-	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) error_handling("connect() error!");
+	if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) error_handling("connect() error!");
 	else printf("Connected...........\n");
-	
+
+
 	while(1) 
 	{
-		memset(message, 0, BUF_SIZE);
-		fputs("Input message(Q to quit): ", stdout);
-		fgets(message, BUF_SIZE, stdin);
-		
-		if(!strcmp(message, "q\n") || !strcmp(message, "Q\n")) break;
+        FD_ZERO(&readfds);
+        FD_SET(sock, &readfds);
 
-		write(sock, message, strlen(message));
-		str_len = read(sock, message, BUF_SIZE);
-		printf("Message from server: %s", message);
-	}
+        // tr.tv_sec = 0;
+        // tr.tv_usec = 500;
+
+        ts.tv_sec = 0;
+        ts.tv_usec = 500000;
+
+        memset(message, 0, BUF_SIZE);
+
+        // printf(". ");
+
+        // if (FD_ISSET(sock, &readfds))
+        // {
+            str_len = read(sock, message, BUF_SIZE);
+            if (str_len < 0) {
+                // printf("\nNo message.\n");
+            }
+            else {
+                printf("\nMessage from server: %s\n", message);
+            }
+        // }
+
+        // // 500usec
+        // if (select(0, &readfds, NULL, NULL, &tr) > 0)
+        // {
+        //     // READ
+        //     str_len = read(sock, message, BUF_SIZE);
+        //     printf("\nMessage from server: %s\n", message);
+        //     fflush(0);
+        // }
+        
+        fflush(0);
+
+        FD_ZERO(&readfds);
+        FD_SET(0, &readfds);  // fd 0 = stdin
+
+        printf("Input message(Q to quit): \n");
+        fflush(0);
+
+        // 2sec
+        if (select(1, &readfds, NULL, NULL, &ts) > 0)
+        {
+            // INPUT
+
+            fgets(message, BUF_SIZE, stdin);
+            if (!strcmp(message, "q\n") || !strcmp(message, "Q\n")) break;
+
+            // SEND
+            write(sock, message, strlen(message));
+
+            // READ
+            str_len = read(sock, message, BUF_SIZE);
+            printf("\nYou sent: %s\n", message);
+            fflush(0);
+        }
+        else {
+            // printf("Timeout.\n");
+            // puts("");
+        }
+
+        fflush(0);
+
+    //     // if (!rstate_active) {
+    //         switch (state)
+    //         {
+    //             case -1:
+    //                 perror("[select] error: ");
+    //                 exit(0);
+    //                 break;
+
+    //             case 0:  // TIME OVER
+    //                 // printf("\nTIME OVER // SWITCHING TO [%s] MODE\n", MODE ? "SEND" : "RECEIVE");
+    //                 // fflush(0);
+
+    //                 // SWITCH MODE
+    //                 MODE = MODE ? SEND : RECEIVE;
+
+    //                 // fputs("\n", stdout);
+    //                 // fflush(0);
+
+    //                 // close(fd);
+    //                 // exit(0);
+    //                 // rstate_active = 1;
+
+    //                 break;
+
+                
+                
+    //             default:
+
+    //                 printf("%s Input message(Q to quit): ", MODE ? "RECEIVE" : "SEND");
+    //                 fflush(0);
+
+    //                 // if (MODE == SEND) {
+    //                     fgets(message, BUF_SIZE, stdin);
+    //                     if (!strcmp(message, "q\n") || !strcmp(message, "Q\n")) break;
+    //                     write(sock, message, strlen(message));
+
+    //                     // READ
+    //                     str_len = read(sock, message, BUF_SIZE);
+    //                     printf("Message from server: %s", message);
+    //                     fflush(0);
+    //                 // }
+
+    //                 // else {  // MODE == RECEIVE
+    //                 //     // READ
+    //                 //     str_len = read(sock, message, BUF_SIZE);
+    //                 //     printf("Message from server: %s", message);
+    //                 //     fflush(0);
+    //                 // }
+
+    //                 break;
+    //         }
+    //     }
+    }
 	
 	close(sock);
 	return 0;
--- mulfd.c
+++ mulfd.c
@@ -11,9 +11,10 @@
 #include <sys/time.h>
 #include <sys/select.h>
 
-#define MAX_BUF     1024
-#define MAX_NAMLEN  30
-#define MAX_SOCK    100
+#define BUF_SIZE        1024
+#define CMDCODE_SIZE    4
+#define NAME_SIZE       30
+#define MAX_SOCKS       100
 
 void error_handling(char *message)
 {
@@ -28,43 +29,43 @@ int main(int argc, char **argv)
         exit(1);
     }
     
-    int serv_sockfd, clnt_sockfd, sockfd, state;
+    int serv_sock, clnt_sock, state;
     struct sockaddr_in clnt_addr, serv_addr;
 
     struct timeval tv;
     fd_set readfds, otherfds, allfds;
     
-    char buf[255];
-    int client[MAX_SOCK];
-    char names[MAX_SOCK][MAX_NAMLEN];
-    int i, j, clnt_sz, clnt_max, fd_max;
+    char buf[BUF_SIZE], message[BUF_SIZE];
+    int client[MAX_SOCKS];
+    char names[MAX_SOCKS][NAME_SIZE];
+    int i, j, clnt_sz, clnt_cnt, fd_max;
 
     state = 0;
 
-    serv_sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (serv_sockfd == -1) error_handling("socket() error");
+    serv_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (serv_sock == -1) error_handling("socket() error");
 
     memset(&serv_addr, 0x00, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(atoi(argv[1]));
 
-    state = bind(serv_sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    state = bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
     if (state == -1) error_handling("bind() error");
 
-    state = listen(serv_sockfd, 5);
+    state = listen(serv_sock, 5);
     if (state == -1) error_handling("listen() error");
 
-    clnt_sockfd = serv_sockfd;
+    clnt_sock = serv_sock;
 
     // INITIALIZE
-    clnt_max = -1;
-    fd_max = serv_sockfd;
+    clnt_cnt = -1;
+    fd_max = serv_sock;
 
-    for (i = 0; i < MAX_SOCK; i++) client[i] = -1;
+    for (i = 0; i < MAX_SOCKS; i++) client[i] = -1;
 
     FD_ZERO(&readfds);
-    FD_SET(serv_sockfd, &readfds);
+    FD_SET(serv_sock, &readfds);
 
     printf("\nStarted TCP Server.\n\n\n");
     fflush(stdout);
@@ -75,59 +76,104 @@ int main(int argc, char **argv)
         state = select(fd_max + 1, &allfds, 0, 0, NULL);
 
         // ACCEPT CLIENT TO SERVER SOCK
-        if (FD_ISSET(serv_sockfd, &allfds))
+        if (FD_ISSET(serv_sock, &allfds))
         {
             clnt_sz = sizeof(clnt_addr);
-            clnt_sockfd = accept(serv_sockfd, (struct sockaddr*)&clnt_addr, &clnt_sz);
+            clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_sz);
             printf("Connection from (%s, %d)\n", inet_ntoa(clnt_addr.sin_addr), ntohs(clnt_addr.sin_port));
 
-            for (i = 0; i < MAX_SOCK; i++)
+            for (i = 0; i < MAX_SOCKS; i++)
             {
                 if (client[i] < 0) {
-                    client[i] = clnt_sockfd;
+                    client[i] = clnt_sock;
                     printf("Client number: %d\n", i + 1);
-                    printf("Client FD: %d\n", clnt_sockfd);
+                    printf("Client FD: %d\n", clnt_sock);
                     break;
                 }
             }
 
-            printf("Accepted [%d]\n", clnt_sockfd);
+            printf("Accepted [%d]\n", clnt_sock);
             printf("===================================\n");
 
-            if (i == MAX_SOCK) perror("Too many clients!\n");
+            if (i == MAX_SOCKS) perror("Too many clients!\n");
 
-            FD_SET(clnt_sockfd, &readfds);
+            FD_SET(clnt_sock, &readfds);
 
-            if (clnt_sockfd > fd_max)   fd_max = clnt_sockfd;
-            if (i > clnt_max)           clnt_max = i;
-            if (--state <= 0)           continue;
+            if (clnt_sock > fd_max) fd_max = clnt_sock;
+            if (i > clnt_cnt)       clnt_cnt = i + 1;
+            if (--state <= 0)       continue;
         }
 
         // CHECK CLIENT SOCKET
-        for (i = 0; i <= clnt_max; i++)
+        for (i = 0; i < clnt_cnt; i++)
         {
-            if ((sockfd = client[i]) < 0) continue;
+            if (client[i] < 0) continue;
 
-            if (FD_ISSET(sockfd, &allfds))
+            if (FD_ISSET(client[i], &allfds))
             {
-                memset(buf, 0, MAX_BUF);
+                memset(buf, 0, BUF_SIZE);
 
                 // DISCONNECT CLIENT
-                if (read(sockfd, buf, MAX_BUF) <= 0)
+                if (read(client[i], buf, BUF_SIZE) <= 0)
                 {
-                    printf("Close sockfd : %d\n", sockfd);
+                    printf("Disconnected client [%d] (%s)\n", client[i], names[i]);
                     printf("===================================\n");
-                    close(sockfd);
-                    FD_CLR(sockfd, &readfds);
+                    fflush(0);
+
+                    close(client[i]);
+                    FD_CLR(client[i], &readfds);
                     client[i] = -1;
-                    memset(names[i], -1, MAX_NAMLEN * sizeof(int));
+                    memset(names[i], 0, NAME_SIZE);
                 }
 
-                else {
-                    printf("RECEIVE [%d]: %s\n", sockfd, buf);
+                else
+                {
+                    int cmdcode = atoi(buf);
 
-                    // SEND RECEIVED MESSAGE TO ORIGINAL SOCKFD
-                    write(sockfd, buf, MAX_BUF);
+                    printf("Received from [%d] (%s): %s %s\n", client[i], names[i], buf, &buf[CMDCODE_SIZE + 1]);
+
+                    if (cmdcode == 1000)  // MODE: SET NAME
+                    {
+                        // CHECK IF REQUESTED NAME IS TAKEN
+                        int taken = 0;
+                        for (j = 0; !taken && j < clnt_cnt; j++)
+                            if (!strcmp(&buf[CMDCODE_SIZE + 1], names[j])) taken = 1;
+
+                        if (taken) {
+                            write(client[i], "0", 2);  // REJECTED
+                        }
+                        else {
+                            write(client[i], "1", 2);  // ACCEPTED
+
+                            memset(names[i], 0, NAME_SIZE);
+                            strcpy(names[i], &buf[CMDCODE_SIZE + 1]);
+                            printf("Set name of client [%d] as [%s]\n", client[i], names[i]);
+                        }
+                    }
+
+                    else if (cmdcode == 2000)  // MODE: MESSAGE
+                    {
+                        // SEND RECEIVED MESSAGE TO ALL CLIENTS
+                        
+                        memset(message, 0, BUF_SIZE);
+
+                        sprintf(message, "%s", names[i]);
+                        sprintf(&message[NAME_SIZE + 1], "%s", &buf[CMDCODE_SIZE + 1]);
+                        
+                        for (j = 0; j < clnt_cnt; j++)
+                        {
+                            // DISCARD DISCONNECTED OR NAMELESS CLIENTS
+                            if (client[j] < 0 || names[j][0] == 0) continue;
+
+                            write(client[j], message, BUF_SIZE);
+                            printf("Sent to client [%d] (%s)\n", client[j], names[j]);
+                        }
+                    }
+
+                    else
+                    {
+                        printf("Error reading cmdcode from [%d]!\n", client[i]);
+                    }
                 }
 
                 if (--state <= 0) break;
